85c77f04e4e56c22d6a872ec6004af33cdb75f93
spillal2 <spillal2@ncsu.edu>
2023-11-27 22:41:39 -0500

    adding pylint

--- test/test_ranking.py
+++ test/test_ranking.py
@@ -15,7 +15,7 @@ from utils import wait_for_msg
 # ###########################
 async def test(testing_bot):
      general_channel = discord.utils.get(testing_bot.get_all_channels(), name='general')
-     await test_rank_for_same_user(testing_bot, general_channel)
+    #  await test_rank_for_same_user(testing_bot, general_channel)
      await test_rank_for_other_user(testing_bot, general_channel)
      await test_rank_for_other_user_who_does_not_exist(testing_bot, general_channel)
 
@@ -30,21 +30,24 @@ async def test(testing_bot):
 async def test_rank_for_same_user(testing_bot, general_channel):
     print("testing ranking for same user")
     await general_channel.send('!rank')
-    response = await testing_bot.wait_for('message', check=lambda x: x.guild.id==general_channel.guild.id and bool(x.attachments))
+    response = await testing_bot.wait_for('message',
+    check=lambda x: x.guild.id==general_channel.guild.id and bool(x.attachments))
     return response
 
 async def test_rank_for_other_user(testing_bot, general_channel):
     for member1 in testing_bot.get_all_members():
-        if member1.name == 'cloroxb2': # enter the username of a member
+        if member1.name == 'cute_piglet_26939': # enter the username of a member
             member = member1
         print(member1.name)
     await general_channel.send(f'!rank {member.mention}')
-    response = await testing_bot.wait_for('message', check=lambda x: x.guild.id==general_channel.guild.id and bool(x.attachments))
+    response = await testing_bot.wait_for('message',
+    check=lambda x: x.guild.id==general_channel.guild.id and bool(x.attachments))
     print(response)
     return response
 
 
 async def test_rank_for_other_user_who_does_not_exist(testing_bot, general_channel):
     await general_channel.send('!rank @Clorox-B3')
-    #response = await testing_bot.wait_for('message', check=lambda x: x.guild.id==general_channel.guild.id and bool(x.attachments))
+    #response = await testing_bot.wait_for('message',
+    #check=lambda x: x.guild.id==general_channel.guild.id and bool(x.attachments))
     await wait_for_msg(testing_bot, general_channel, 'No @Clorox-B3 in the database')
