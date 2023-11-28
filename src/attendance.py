95a65ca25736384c4623b1de61e702ba93e66c91
VaishnaviNaik96 <111774368+VaishnaviNaik96@users.noreply.github.com>
2023-11-27 22:27:28 -0500

    Update attendance.py

--- src/attendance.py
+++ src/attendance.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2023 G.I.T.S.3 - All Rights Reserved
+Copyright (C) 2023 TeachersPetBotv2.0 - All Rights Reserved
 You may use, distribute, and modify this code under the
 terms of the MIT license.
 You should have received a copy of the MIT license with
